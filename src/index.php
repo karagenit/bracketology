3dec04581fc827cfdcb61591f8af64b76c90085e
Caleb Smith <karagenit@outlook.com>
2018-03-10 14:37:50 -0500

    Add Main CSS File

--- src/index.php
+++ src/index.php
@@ -12,6 +12,7 @@
     <title>CHS Stats Bracketology</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="./res/css/main.css">
   </head>
   <body>
     <h1>CHS Stats Bracketology</h1>
