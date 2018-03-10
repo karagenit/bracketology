52ac491e41da00c057a2cf0b5a31b139c2eaf016
Caleb Smith <karagenit@outlook.com>
2018-03-10 15:36:35 -0500

    Add Meta Head Include

--- src/index.php
+++ src/index.php
@@ -1,19 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <meta name="description" content="CHS Stats Bracketology">
-    <meta name="author" content="Caleb Smith">
-    <link rel="icon" href="./res/img/favicon.png">
-
-    <title>CHS Stats Bracketology</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="./res/css/main.css">
-  </head>
+  <?php echo file_get_contents("./includes/head.html") ?>
   <body>
     <?php echo file_get_contents("./includes/header.html") ?>
   </body>
