aabd85f21e348de5a1ab35c18ea17c0bc7a97c69
Caleb Smith <karagenit@outlook.com>
2018-03-10 15:50:40 -0500

    Add Home Content

--- src/index.php
+++ src/index.php
@@ -3,5 +3,43 @@
   <?php echo file_get_contents("./includes/head.html") ?>
   <body>
     <?php echo file_get_contents("./includes/header.html") ?>
+
+    <p>Since 2004 the Carmel High School AP Statistics classes have collected data on the NCAA Men’s March Madness basketball tournament. The goal has been to identify the impact different variables have on the ability to successfully pick tournament games such as basketball knowledge, age and gender.</p>
+
+    <p>This data will be analyzed by students in AP Stats as a review assignment in preparation for their AP Exam. Please take a few minutes to submit your bracket by selecting the link above. You may view updated standings as the tournament progresses.</p>
+
+    <h5>Scoring</h5>
+    <table class="table table-bordered">
+      <tr>
+        <th>Round</th>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+      </tr>
+      <tr>
+        <th>Points</th>
+        <td>1</td>
+        <td>2</td>
+        <td>4</td>
+        <td>6</td>
+        <td>8</td>
+        <td>10</td>
+      </tr>
+    </table>
+
+    <h5>Prior Winners</h5>
+    <table class="table">
+      <tr>
+        <th>Year</th>
+        <th>Name</th>
+      </tr>
+      <tr>
+        <td>2018</td>
+        <td>???</td>
+      </tr>
+    </table>
   </body>
 </html>
