cf93ffb95255fd395d28a97c6896beb23c72398e
Caleb Smith <karagenit@outlook.com>
2018-03-10 16:19:20 -0500

    Add Basic Form

---
+++ src/bracket.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php echo file_get_contents("./includes/head.html") ?>
+  <body>
+    <?php echo file_get_contents("./includes/header.html") ?>
+
+    <form action="submit.php">
+      <div class="form-row">
+        <h5>Name</h5>
+      </div>
+      <div class="form-row">
+        <div class="form-group col">
+          <input type="text" name="firstname" class="form-control" placeholder="First">
+        </div>
+        <div class="form-group col">
+          <input type="text" name="lastname" class="form-control" placeholder="Last">
+        </div>
+      </div>
+      <div class="form-row">
+        <button type="submit" class="btn btn-success">Submit</button>
+      </div>
+    </form>
+  </body>
+</html>
