3fddc0816fdc25b8ee8c611d46eb9cb54a05aa45
Caleb Smith <karagenit@outlook.com>
2018-03-10 14:34:57 -0500

    Add Index

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="CHS Stats Bracketology">
+    <meta name="author" content="Caleb Smith">
+    <link rel="icon" href="./res/img/favicon.ico">
+
+    <title>CHS Stats Bracketology</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>CHS Stats Bracketology</h1>
+  </body>
+</html>
