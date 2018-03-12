a55ce9d47ff993ce64ed448d2217778ebac6e248
Caleb Smith <karagenit@outlook.com>
2018-03-12 00:52:04 -0400

    Dump Bracket Data to JSON Files

--- src/submit.php
+++ src/submit.php
@@ -2,6 +2,10 @@
 <html lang="en">
   <?php echo file_get_contents("./includes/head.html") ?>
   <body>
-    <?php var_dump($_POST) ?>
+    <?php
+      file_put_contents("./brackets/" . microtime() . ".json", json_encode($_POST));
+      echo file_get_contents("./includes/header.html");
+    ?>
+    <h3>Bracket Saved!</h3>
   </body>
 </html>
