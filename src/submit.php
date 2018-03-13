53761a8e2d15b0d000fedac8a29a7bf9d22f0358
Caleb Smith <karagenit@outlook.com>
2018-03-12 23:36:20 -0400

    Save Brackets to SQL
    Closes #6

--- src/submit.php
+++ src/submit.php
@@ -5,19 +5,24 @@
     <?php
       echo file_get_contents("./includes/header.html");
 
+      var_dump($_POST);
+
+      // TODO read file or print current user
       $db = new mysqli("localhost", "caleb", "", "bracket");
       $stmt = $db->prepare("INSERT INTO brackets (firstname, lastname, age, gender, school, knowledge, email, bracket) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
       $stmt->bind_param("ssississ", $firstname, $lastname, $age, $gender, $school, $knowledge, $email, $bracket);
 
       // TODO use POST fields - convert to #s, and grab all bracket info
-      $firstname = "caleb";
-      $lastname = "smith";
-      $age = 3;
-      $gender = "M";
-      $school = "CHS";
-      $knowledge = 999;
-      $email = "no";
-      $bracket = "{test: data}";
+      $firstname = $_POST["firstname"];
+      $lastname = $_POST["lastname"];
+      $age = intval($_POST["age"]);
+      $gender = $_POST["gender"];
+      $school = $_POST["school"];
+      $knowledge = intval($_POST["knowledge"]);
+      $email = $_POST["email"];
+      $bracket = json_encode(array_filter($_POST, function($value, $key) {
+        return strpos($key, "_") !== false; // key contains an underscore
+      }, ARRAY_FILTER_USE_BOTH));
 
       $success = $stmt->execute();
 
