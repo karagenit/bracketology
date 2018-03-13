b5ee5dbb46b4775b6bebab4c336b1e3f00b8ab74
Caleb Smith <karagenit@outlook.com>
2018-03-12 23:22:26 -0400

    Add Basic SQL Connection/Insert

--- src/submit.php
+++ src/submit.php
@@ -3,9 +3,30 @@
   <?php echo file_get_contents("./includes/head.html") ?>
   <body>
     <?php
-      file_put_contents("./brackets/" . microtime() . ".json", json_encode($_POST));
       echo file_get_contents("./includes/header.html");
+
+      $db = new mysqli("localhost", "caleb", "", "bracket");
+      $stmt = $db->prepare("INSERT INTO brackets (firstname, lastname, age, gender, school, knowledge, email, bracket) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+      $stmt->bind_param("ssississ", $firstname, $lastname, $age, $gender, $school, $knowledge, $email, $bracket);
+
+      // TODO use POST fields - convert to #s, and grab all bracket info
+      $firstname = "caleb";
+      $lastname = "smith";
+      $age = 3;
+      $gender = "M";
+      $school = "CHS";
+      $knowledge = 999;
+      $email = "no";
+      $bracket = "{test: data}";
+
+      $success = $stmt->execute();
+
+      if ($success) {
+        echo "Bracket Saved!";
+      } else {
+        echo "Error saving...";
+        // TODO print error info
+      }
     ?>
-    <h3>Bracket Saved!</h3>
   </body>
 </html>
