9fde44b9b6aecc554de9c84a4d4c562f4c2d9410
Caleb Smith <karagenit@outlook.com>
2018-03-12 23:06:18 -0400

    Add DB Setup Script

---
+++ db/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mysql -e "CREATE DATABASE IF NOT EXISTS bracket;"
+mysql bracket -e "CREATE TABLE IF NOT EXISTS brackets (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, firstname VARCHAR(64) NOT NULL, lastname VARCHAR(64) NOT NULL, \
+                  age INT NOT NULL, gender CHAR(1) NOT NULL, school VARCHAR(64) NOT NULL, knowledge INT NOT NULL, email VARCHAR(64), bracket TEXT NOT NULL);"
