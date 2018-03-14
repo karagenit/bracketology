4a33bb39e7e09807945fa6ac9a0cb0bf1e7abfd5
Caleb Smith <karagenit@outlook.com>
2018-03-14 08:15:57 -0400

    Add 404 & Redirect

---
+++ src/404.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php echo file_get_contents("./includes/head.html") ?>
+  <body>
+    <?php echo file_get_contents("./includes/header.html") ?>
+    <h3>Page not found!</h3>
+  </body>
+</html>
